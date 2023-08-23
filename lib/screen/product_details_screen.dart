7ee6354906eea560b89d194e0fc19022c5ad874c
Sudipta Gupta <130888739+sudiptagupta17@users.noreply.github.com>
2023-08-23 09:40:12 +0530

    product screen ui create

--- lib/screen/product_details_screen.dart
+++ lib/screen/product_details_screen.dart
@@ -55,7 +55,7 @@ class ProductDetails extends StatelessWidget {
                                 )
                        ],
                      ),
-                     SizedBox(height: 17,)
+                     SizedBox(height: 17,),
                    ],
                  ),
               )
